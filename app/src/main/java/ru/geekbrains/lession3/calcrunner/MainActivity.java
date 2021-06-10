b1f220144f343df31a9c6b2ae7846ba625a6b2a6
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-06-10 14:17:11 +0300

    Task 3 well done. Create new application for run calculator and their settings.

--- app/src/main/java/ru/geekbrains/lession3/calcrunner/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lession3/calcrunner/MainActivity.java
@@ -2,13 +2,43 @@ package ru.geekbrains.lession3.calcrunner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonsInit();
+    }
+
+    private void buttonsInit()
+    {
+        Button runCalcButton = findViewById(R.id._run_button);
+        runCalcButton.setOnClickListener(this);
+        Button runSettingsButton = findViewById(R.id._run_settings_button);
+        runSettingsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id._run_button:
+                Intent runCalculator = new Intent("ru.geekbrains.lession5.calculate");
+                startActivity(runCalculator);
+                break;
+            case R.id._run_settings_button:
+                Intent runSettingsCalculator = new Intent("ru.geekbrains.lession5.calculate.settings");
+                startActivity(runSettingsCalculator);
+                break;
+            default:
+                break;
+        }
     }
 }
