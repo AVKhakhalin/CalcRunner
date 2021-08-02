be2f66ed4453a9bcbaa38849025ff4b7e71525f0
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-08-02 19:35:50 +0300

    Выполнен 3 пункт ТЗ. Создано приложение для запуска калькулятора, размещённого в репозитории: https://github.com/AVKhakhalin/Calculator

--- app/src/main/java/ru/geekbrains/lession3/calcrunner/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lession3/calcrunner/MainActivity.java
@@ -1,11 +1,12 @@
 package ru.geekbrains.lession3.calcrunner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,8 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonsInit();
     }
 
-    private void buttonsInit()
-    {
+    private void buttonsInit() {
         Button runCalcButton = findViewById(R.id._run_button);
         runCalcButton.setOnClickListener(this);
         Button runSettingsButton = findViewById(R.id._run_settings_button);
@@ -27,15 +27,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
+        ActivityInfo activityInfo;
+        switch (v.getId()) {
             case R.id._run_button:
-                Intent runCalculator = new Intent("ru.geekbrains.lession5.calculate");
-                startActivity(runCalculator);
+                Intent runCalculator = new Intent("ru.geekbrains.lessions2345.calculator");
+                activityInfo = runCalculator.resolveActivityInfo(getPackageManager(), runCalculator.getFlags());
+                if (activityInfo != null) {
+                    startActivity(runCalculator);
+                }
                 break;
             case R.id._run_settings_button:
-                Intent runSettingsCalculator = new Intent("ru.geekbrains.lession5.calculate.settings");
-                startActivity(runSettingsCalculator);
+                Intent runSettingsCalculator = new Intent("ru.geekbrains.lessions2345.calculator.settings");
+                activityInfo = runSettingsCalculator.resolveActivityInfo(getPackageManager(), runSettingsCalculator.getFlags());
+                if (activityInfo != null) {
+                    startActivity(runSettingsCalculator);
+                }
                 break;
             default:
                 break;
